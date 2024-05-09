4a8174d697ccc3953a3336f2bcf9b982d32ba063
zbdeng <dengzhibin@innoinstrument.com.cn>
2024-05-09 13:27:49 +0800

    添加数据库依赖

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    // room
+    implementation(libs.androidx.room)
+    annotationProcessor(libs.androidx.room.compiler)
 }
