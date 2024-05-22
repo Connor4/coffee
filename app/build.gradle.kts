f6d790d412c73e45f1baa0330eaa7ca7ecfb5524
zbdeng <dengzhibin@innoinstrument.com.cn>
2024-05-22 17:08:37 +0800

    增加room依赖

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.kapt)
 }
 
 android {
@@ -59,6 +60,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -67,6 +69,6 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
     // room
-    implementation(libs.androidx.room)
-    annotationProcessor(libs.androidx.room.compiler)
+    implementation(libs.room.ktx)
+    kapt(libs.room.compiler)
 }
