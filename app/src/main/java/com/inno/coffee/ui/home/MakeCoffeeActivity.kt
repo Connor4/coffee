5ccab348ee63bcab75f7df52d21af653c3c3606d
zbdeng <dengzhibin@innoinstrument.com.cn>
2024-05-22 11:45:25 +0800

    增加饮品制作页

---
+++ app/src/main/java/com/inno/coffee/ui/home/MakeCoffeeActivity.kt
@@ -0,0 +1,13 @@
+package com.inno.coffee.ui.home
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+/**
+ * 饮品制作页
+ */
+class MakeCoffeeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
