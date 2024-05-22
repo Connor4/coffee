06699e4badf1a12dbfd8d539f995c0bf71b19d5b
zbdeng <dengzhibin@innoinstrument.com.cn>
2024-05-22 13:24:03 +0800

    添加后台操作页面

---
+++ app/src/main/java/com/inno/coffee/ui/settings/SettingActivity.kt
@@ -0,0 +1,16 @@
+package com.inno.coffee.ui.settings
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+/**
+ * 后台操作页面
+ */
+class SettingActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
