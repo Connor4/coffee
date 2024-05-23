3315eac6bfd7387afceaa669cd7cf3fa344a69b1
zbdeng <dengzhibin@innoinstrument.com.cn>
2024-05-23 09:06:42 +0800

    添加首次启动界面处理逻辑

--- app/src/main/java/com/inno/coffee/CoffeeApplication.kt
+++ app/src/main/java/com/inno/coffee/CoffeeApplication.kt
@@ -1,11 +1,26 @@
 package com.inno.coffee
 
 import android.app.Application
+import com.inno.coffee.data.PreferencesManager
 
 class CoffeeApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        init()
+        delayInit()
+        dependenceInit()
+    }
+
+    private fun init() {
+        PreferencesManager.getInstance(this)
+    }
+
+    private fun delayInit() {
+
+    }
+
+    private fun dependenceInit() {
 
     }
 
