89f9fc8b9efdd53dcd262f23495753688be46b6f
zbdeng <dengzhibin@innoinstrument.com.cn>
2024-06-07 11:41:12 +0800

    统一kotlinx序列化依赖版本号
    (cherry picked from commit ce9c4ef435a7d3f1bc16d239fce046a924f52640)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ plugins {
     alias(libs.plugins.kapt) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.android.library) apply false
-    kotlin("jvm") version "2.0.0-RC3" apply false
-    kotlin("plugin.serialization") version "2.0.0-RC3" apply false
+    kotlin("jvm") version libs.versions.kotlin.get() apply false
+    kotlin("plugin.serialization") version libs.versions.kotlin.get() apply false
 }
 
 extra.apply {
