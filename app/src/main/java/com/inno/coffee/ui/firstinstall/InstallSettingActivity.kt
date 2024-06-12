8c101105bd619cd5a5c14f4caa25bb3079de6c8b
zbdeng <dengzhibin@innoinstrument.com.cn>
2024-06-12 09:31:52 +0800

    新增临时使用权限请求逻辑
    (cherry picked from commit 4eb6b2bc32874a01c3ee0dbec48b6fb2c1c224fe)

--- app/src/main/java/com/inno/coffee/ui/firstinstall/InstallSettingActivity.kt
+++ app/src/main/java/com/inno/coffee/ui/firstinstall/InstallSettingActivity.kt
@@ -1,8 +1,12 @@
 package com.inno.coffee.ui.firstinstall
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.inno.coffee.ui.home.launchMakeCoffeeActivity
 import com.inno.coffee.ui.theme.CoffeeTheme
 
@@ -29,6 +33,7 @@ class InstallSettingActivity : ComponentActivity() {
         } else {
             launchMakeCoffeeActivity(this)
         }
+        requestPermissions()
     }
 
     override fun finish() {
@@ -36,42 +41,30 @@ class InstallSettingActivity : ComponentActivity() {
         overridePendingTransition(0, 0)
     }
 
-//    private val REQUEST_CODE_PERMISSIONS = 1001
-//
-//    // 检查并请求权限
-//    private fun requestPermissions() {
-//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
-//            ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-//
-//            ActivityCompat.requestPermissions(
-//                this,
-//                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE),
-//                REQUEST_CODE_PERMISSIONS
-//            )
-//        } else {
-//            // 权限已授予，可以执行相应操作
-//            loadFromSdCard()
-//        }
-//    }
-//
-//    // 处理权限请求结果
-//    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-//        if (requestCode == REQUEST_CODE_PERMISSIONS) {
-//            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-//                // 权限授予
-//                loadFromSdCard()
-//            } else {
-//                // 权限拒绝，处理相应逻辑
-//                Toast.makeText(this, "权限被拒绝，无法读取SD卡。", Toast.LENGTH_SHORT).show()
-//            }
-//        }
-//    }
-//
-//    // 示例方法，显示如何调用请求权限方法
-//    private fun onSomeEventTriggered() {
-//        requestPermissions()
-//    }
+    private val REQUEST_CODE_PERMISSIONS = 1001
 
+    // 检查并请求权限
+    private fun requestPermissions() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE), REQUEST_CODE_PERMISSIONS)
+        }
+    }
+
+    // 处理权限请求结果
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
+        grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+//            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+//            } else {
+//            }
+        }
+    }
 
 }
