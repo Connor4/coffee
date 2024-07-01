ee1ec1588981f8e73d85d4f7d46c691025a86d9f
zbdeng <dengzhibin@innoinstrument.com.cn>
2024-07-01 16:35:44 +0800

    首次安装选择语言、日期逻辑修改

--- app/src/main/java/com/inno/coffee/ui/firstinstall/FirstInstallContent.kt
+++ app/src/main/java/com/inno/coffee/ui/firstinstall/FirstInstallContent.kt
@@ -4,12 +4,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.selection.selectableGroup
@@ -129,7 +129,7 @@ fun DatePickerPage(onDatePick: () -> Unit) {
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
         Column(modifier = Modifier
             .width(1100.dp)
-            .wrapContentHeight()) {
+            .fillMaxHeight()) {
             val datePickerState = rememberDatePickerState(initialSelectedDateMillis = 1704124800000)
             DatePicker(
                 state = datePickerState,
