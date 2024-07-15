e1b46b4fc92b25f2d71df80e56969f00289a29c5
zbdeng <dengzhibin@innoinstrument.com.cn>
2024-07-15 09:31:54 +0800

    删除代码

--- app/src/main/java/com/inno/coffee/data/firstinstall/InstallViewModel.kt
+++ app/src/main/java/com/inno/coffee/data/firstinstall/InstallViewModel.kt
@@ -5,13 +5,10 @@ import androidx.lifecycle.viewModelScope
 import com.inno.coffee.di.DefaultDispatcher
 import com.inno.coffee.utilities.CoffeeDataStore
 import com.inno.common.db.entity.defaultUsers
-import com.inno.common.utils.Logger
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import java.io.DataOutputStream
-import java.util.Calendar
 import javax.inject.Inject
 
 @HiltViewModel
@@ -34,28 +31,6 @@ class InstallViewModel @Inject constructor(
         viewModelScope.launch {
             withContext(defaultDispatcher) {
                 dataStore.saveMachineLanguage(language)
-
-                val calendar = Calendar.getInstance().apply {
-                    timeInMillis = date
-                    set(Calendar.HOUR_OF_DAY, hour)
-                    set(Calendar.MINUTE, min)
-                    set(Calendar.SECOND, 0)
-                    set(Calendar.MILLISECOND, 0)
-                }
-                val timeInMillis = calendar.timeInMillis
-
-                try {
-                    val process = Runtime.getRuntime().exec("su")
-                    val outputStream = DataOutputStream(process.outputStream)
-                    outputStream.writeBytes("date -s @${timeInMillis / 1000}\n")
-                    outputStream.writeBytes("exit\n")
-                    outputStream.flush()
-                    process.waitFor()
-                    Logger.d("SetTime", "System time set to $timeInMillis using root")
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                    Logger.e("SetTime", "Failed to set system time with root: ${e.message}")
-                }
             }
         }
     }
