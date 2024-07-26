9355fc33dd7f9cce3fb5b0e2ad62d5e3f4799b5b
zbdeng <dengzhibin@innoinstrument.com.cn>
2024-07-26 16:01:52 +0800

    修改注释

--- serialport/src/main/java/com/inno/serialport/utilities/PullBufInfo.kt
+++ serialport/src/main/java/com/inno/serialport/utilities/PullBufInfo.kt
@@ -4,3 +4,15 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class PullBufInfo(val id: Int, val pollBuf: ByteArray = byteArrayOf())
+
+// poll指令
+//7e 02 01 02 00 01 00 cc 2b 7e //send
+//7E 02 02 04 00 01 00 00 00 4F 4C 7E //reply
+//7E //包头
+//02 //地址
+//02 //控制
+//04 00 //长度
+//64 00 //指令
+//00 00 //结果： 0：success, 其他： fault
+//FC BB //crc
+//7E //reply
