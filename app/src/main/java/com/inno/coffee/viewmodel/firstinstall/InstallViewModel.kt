a0b615fcac07dbbc81b504892643d15270148c47
zbdeng <dengzhibin@innoinstrument.com.cn>
2024-08-06 14:26:17 +0800

    优化逻辑

--- app/src/main/java/com/inno/coffee/viewmodel/firstinstall/InstallViewModel.kt
+++ app/src/main/java/com/inno/coffee/viewmodel/firstinstall/InstallViewModel.kt
@@ -3,33 +3,28 @@ package com.inno.coffee.viewmodel.firstinstall
 import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.inno.coffee.di.DefaultDispatcher
 import com.inno.coffee.function.defaultsetting.DefaultSettingManager
 import com.inno.common.utils.CoffeeDataStore
 import com.inno.common.utils.CoffeeSharedPreferences
 import com.inno.common.utils.SystemLocaleHelper
 import com.inno.common.utils.TimeUtils
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
 class InstallViewModel @Inject constructor(
     private val dataStore: CoffeeDataStore,
-    @DefaultDispatcher private val defaultDispatcher: CoroutineDispatcher
+//    @DefaultDispatcher private val defaultDispatcher: CoroutineDispatcher
 ) : ViewModel() {
 
     fun finishSetting(context: Context, date: Long, hour: Int, min: Int, language: String) {
+        TimeUtils.setDateAndTime(context, date, hour, min)
         viewModelScope.launch {
-            withContext(defaultDispatcher) {
-                DefaultSettingManager.insertDefaultUser()
-                CoffeeSharedPreferences.getInstance().isFirstInstall = false
-                TimeUtils.setDateAndTime(context, date, hour, min)
-                SystemLocaleHelper.changeSystemLocale(context, language)
-                dataStore.saveMachineLanguage(language)
-            }
+            SystemLocaleHelper.changeSystemLocale(context, language)
+            CoffeeSharedPreferences.getInstance().isFirstInstall = false
+            dataStore.saveMachineLanguage(language)
+            DefaultSettingManager.insertDefaultUser()
         }
     }
 
