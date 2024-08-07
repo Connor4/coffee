096b093010eb1163f0f2d25f0e976fb09ebc6d33
zbdeng <dengzhibin@innoinstrument.com.cn>
2024-08-07 09:47:39 +0800

    添加显示模块

---
+++ app/src/main/java/com/inno/coffee/ui/settings/display/DisplayActivity.kt
@@ -0,0 +1,14 @@
+package com.inno.coffee.ui.settings.display
+
+import android.os.Bundle
+import com.inno.coffee.ui.base.CoffeeActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class DisplayActivity : CoffeeActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
