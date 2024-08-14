6cbc7d1f48a4ed47053e16b302cc6ec9eb31259b
zbdeng <dengzhibin@innoinstrument.com.cn>
2024-08-14 15:22:36 +0800

    新增pull数据类型

--- serialport/src/main/java/com/inno/serialport/utilities/statusenum/BoilerStatusEnum.kt
+++ serialport/src/main/java/com/inno/serialport/utilities/statusenum/BoilerStatusEnum.kt
@@ -4,7 +4,9 @@ enum class BoilerStatusEnum(val value: Int) {
     FAIL(-1),
     LEFT_BOILER_TEMPERATURE(1006),
     RIGHT_BOILER_TEMPERATURE(1007),
-    STREAM_BOILER_TEMPERATURE(1008);
+    STREAM_BOILER_TEMPERATURE(1008),
+    LEFT_GRINDER_POWDER_DOSAGE(1009),
+    RIGHT_GRINDER_POWDER_DOSAGE(1010);
 
     companion object {
         fun getStatus(value: Int): BoilerStatusEnum {
