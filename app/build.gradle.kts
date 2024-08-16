9834f7cc74106749e0dce17956dfed857c8a2422
zbdeng <dengzhibin@innoinstrument.com.cn>
2024-08-16 15:18:50 +0800

    添加viewpager2依赖

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,6 +82,7 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.fragment)
+    implementation(libs.androidx.viewpager2)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
