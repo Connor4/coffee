4d00fde4be3fd8cd1295d8a725fedc27e3e6547a
zbdeng <dengzhibin@innoinstrument.com.cn>
2024-08-27 10:18:16 +0800

    设置activity为强制横屏模式，解决跳转页面先竖屏再横屏问题

--- app/src/main/java/com/inno/coffee/ui/base/CoffeeActivity.kt
+++ app/src/main/java/com/inno/coffee/ui/base/CoffeeActivity.kt
@@ -1,8 +1,15 @@
 package com.inno.coffee.ui.base
 
+import android.content.pm.ActivityInfo
+import android.os.Bundle
 import androidx.activity.ComponentActivity
 
 open class CoffeeActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+    }
 
     override fun finish() {
         super.finish()
