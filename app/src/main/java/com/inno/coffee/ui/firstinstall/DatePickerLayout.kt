275aeaeb0756a56e6aa94c63ef2b61a3a0dbb30b
zbdeng <dengzhibin@innoinstrument.com.cn>
2024-09-03 15:11:20 +0800

    添加日期选择页

--- app/src/main/java/com/inno/coffee/ui/firstinstall/DatePickerLayout.kt
+++ app/src/main/java/com/inno/coffee/ui/firstinstall/DatePickerLayout.kt
@@ -1,6 +1,5 @@
 package com.inno.coffee.ui.firstinstall
 
-import android.widget.DatePicker
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -11,7 +10,12 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDefaults
+import androidx.compose.material3.DatePickerFormatter
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -20,6 +24,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -27,8 +32,8 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.viewinterop.AndroidView
 import com.inno.coffee.R
+import com.inno.coffee.utilities.DEFAULT_SYSTEM_TIME
 import com.inno.coffee.utilities.nsp
 
 
@@ -39,8 +44,7 @@ fun DatePickerLayout(modifier: Modifier = Modifier, onDatePick: (Long?) -> Unit)
     }
 
     Box(
-//        modifier = modifier,
-        modifier = Modifier.background(Color.LightGray)
+        modifier = modifier.background(Color.LightGray),
     ) {
         Box(
             modifier = Modifier
@@ -58,13 +62,7 @@ fun DatePickerLayout(modifier: Modifier = Modifier, onDatePick: (Long?) -> Unit)
             )
         }
 
-        AndroidView(
-            modifier = Modifier.padding(start = 50.dp, top = 155.dp, end = 50.dp, bottom = 100.dp),
-            factory = {
-                DatePicker(it).apply {
-
-                }
-            })
+        CustomDatePicker()
 
         Button(
             onClick = {
@@ -93,6 +91,42 @@ fun DatePickerLayout(modifier: Modifier = Modifier, onDatePick: (Long?) -> Unit)
             )
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun CustomDatePicker() {
+    val dateFormatter: DatePickerFormatter = remember { DatePickerDefaults.dateFormatter() }
+    val datePickerState =
+        rememberDatePickerState(
+            initialSelectedDateMillis = (DEFAULT_SYSTEM_TIME + 86400000))
+    DatePicker(
+        state = datePickerState,
+        title = {
+            Text(
+                text = "*Select Date",
+                fontSize = 7.nsp(),
+                fontWeight = FontWeight.Bold,
+                color = Color.White,
+            )
+        },
+        headline = {
+            val defaultLocale = LocalConfiguration.current.locales[0]
+            val formattedDate = dateFormatter.formatDate(
+                dateMillis = datePickerState.selectedDateMillis,
+                locale = defaultLocale,
+            )
+            Text(
+                text = formattedDate ?: "",
+                maxLines = 1,
+                fontSize = 10.nsp(),
+                fontWeight = FontWeight.Bold,
+                color = Color.White,
+            )
+        },
+//        showModeToggle = false,
+        modifier = Modifier.padding(start = 50.dp, top = 155.dp, end = 50.dp, bottom = 100.dp)
+    )
 }
 
 
