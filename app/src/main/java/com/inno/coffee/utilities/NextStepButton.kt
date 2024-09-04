b3964e423283e4c701028e1a4cb64bfec393c88f
zbdeng <dengzhibin@innoinstrument.com.cn>
2024-09-04 16:14:47 +0800

    修改button selector效果实现方式，不需要使用实验api

--- app/src/main/java/com/inno/coffee/utilities/NextStepButton.kt
+++ app/src/main/java/com/inno/coffee/utilities/NextStepButton.kt
@@ -1,7 +1,8 @@
 package com.inno.coffee.utilities
 
-import android.view.MotionEvent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
@@ -11,51 +12,37 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.inno.coffee.R
 
-@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun NextStepButton(
     modifier: Modifier = Modifier,
     onNextClick: () -> Unit = {},
 ) {
-    var isPressed by remember {
-        mutableStateOf(false)
+    val interactionSource = remember {
+        MutableInteractionSource()
     }
+    val isPressed by interactionSource.collectIsPressedAsState()
+
     Box(
         modifier = modifier
     ) {
         Button(
             onClick = {
-
+                onNextClick()
             },
             colors = ButtonDefaults.buttonColors(
                 containerColor = Color.Transparent
             ),
+            interactionSource = interactionSource,
             modifier = Modifier
                 .padding(16.dp)
-                .pointerInteropFilter {
-                    when (it.action) {
-                        MotionEvent.ACTION_DOWN -> {
-                            isPressed = true
-                        }
-                        MotionEvent.ACTION_UP -> {
-                            isPressed = false
-                            onNextClick()
-                        }
-                    }
-                    true
-                }
         ) {
             Text(
                 text = stringResource(id = R.string.first_install_next),
