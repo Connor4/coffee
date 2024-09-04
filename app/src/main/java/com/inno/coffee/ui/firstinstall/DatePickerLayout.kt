1331a44ef8de00fdba6fcad0d0d768adf831f94e
zbdeng <dengzhibin@innoinstrument.com.cn>
2024-09-04 16:05:48 +0800

    抽取next step按钮作为公用类

--- app/src/main/java/com/inno/coffee/ui/firstinstall/DatePickerLayout.kt
+++ app/src/main/java/com/inno/coffee/ui/firstinstall/DatePickerLayout.kt
@@ -11,8 +11,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDefaults
 import androidx.compose.material3.DatePickerFormatter
@@ -21,10 +19,8 @@ import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -39,15 +35,13 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.inno.coffee.R
 import com.inno.coffee.utilities.DEFAULT_SYSTEM_TIME
+import com.inno.coffee.utilities.NextStepButton
 import com.inno.coffee.utilities.fastclickWithoutRipple
 import com.inno.coffee.utilities.nsp
 
 
 @Composable
 fun DatePickerLayout(modifier: Modifier = Modifier, onDatePick: (Long?) -> Unit) {
-    var isPressed by remember {
-        mutableStateOf(false)
-    }
     val datePickerViewRef = remember {
         mutableStateOf<CoffeeDatePickerView?>(null)
     }
@@ -137,31 +131,8 @@ fun DatePickerLayout(modifier: Modifier = Modifier, onDatePick: (Long?) -> Unit)
                 .background(Color(0xFF191A1D)))
         }
 
-        Button(
-            onClick = {
-                isPressed = true
-//                onDatePick(datePickerState.selectedDateMillis)
-            },
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color.Transparent
-            ),
-            modifier = Modifier
-                .align(Alignment.BottomEnd)
-                .padding(16.dp)
-        ) {
-            Text(
-                text = stringResource(id = R.string.first_install_next),
-                fontSize = 7.nsp(),
-                color = Color.White
-            )
-            Spacer(modifier = Modifier.width(13.dp))
-            Image(
-                painter = if (isPressed) painterResource(
-                    id = R.drawable.install_language_next_pressed_ic)
-                else painterResource(
-                    id = R.drawable.install_language_next_normal_ic),
-                contentDescription = null,
-            )
+        NextStepButton(modifier = Modifier.align(Alignment.BottomEnd)) {
+//            onDatePick(datePickerState.selectedDateMillis)
         }
     }
 }
