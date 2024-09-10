968c7e906099adaec152c41a3b560f8e34f5bfa2
zbdeng <dengzhibin@innoinstrument.com.cn>
2024-09-10 09:19:58 +0800

    添加product file对应16进制component id

--- serialport/src/main/java/com/inno/serialport/utilities/profile/ComponentConst.kt
+++ serialport/src/main/java/com/inno/serialport/utilities/profile/ComponentConst.kt
@@ -1,18 +1,18 @@
 package com.inno.serialport.utilities.profile
 
-const val LEFT_BREWER_ID: Short = 1000
-const val RIGHT_BREWER_ID: Short = 1001
-const val LEFT_BOILER_ID: Short = 2000
-const val RIGHT_BOILER_ID: Short = 2001
-const val STEAM_BOILER_ID: Short = 2002
-const val FRONT_GRINDER_ID: Short = 3000
-const val BACK_GRINDER_ID: Short = 3001
-const val WATER_INPUT_VALVE_ID: Short = 4000
-const val LEFT_VALVE_LEFT_BOILER_ID: Short = 4001
-const val MIDILE_VALVE_LEFT_BOILER_ID: Short = 4002
-const val RIGHT_VALVE_LEFT_BOILER_ID: Short = 4003
-const val LEFT_VALVE_RIGHT_BOILER_ID: Short = 4004
-const val MIDILE_VALVE_RIGHT_BOILER_ID: Short = 4005
-const val RIGHT_VALVE_RIGHT_BOILER_ID: Short = 4006
-const val WATER_INPUT_PUMP_ID: Short = 5000
-const val AIR_INPUT_PUMP_ID: Short = 5100
+const val LEFT_BREWER_ID: Short = 1000 // 0x3e8
+const val RIGHT_BREWER_ID: Short = 1001 // 0x3e9
+const val LEFT_BOILER_ID: Short = 2000 // 0x7d0
+const val RIGHT_BOILER_ID: Short = 2001 // 0x7d1
+const val STEAM_BOILER_ID: Short = 2002 // 0x7d2
+const val FRONT_GRINDER_ID: Short = 3000 // 0xbb8
+const val BACK_GRINDER_ID: Short = 3001 //0xbb9
+const val WATER_INPUT_VALVE_ID: Short = 4000 // 0xfa0
+const val LEFT_VALVE_LEFT_BOILER_ID: Short = 4001 // 0xfa1
+const val MIDILE_VALVE_LEFT_BOILER_ID: Short = 4002 // 0xfa2
+const val RIGHT_VALVE_LEFT_BOILER_ID: Short = 4003 // 0xfa3
+const val LEFT_VALVE_RIGHT_BOILER_ID: Short = 4004 // 0xfa4
+const val MIDILE_VALVE_RIGHT_BOILER_ID: Short = 4005 // 0xfa5
+const val RIGHT_VALVE_RIGHT_BOILER_ID: Short = 4006 // 0xfa6
+const val WATER_INPUT_PUMP_ID: Short = 5000 // 0x1388
+const val AIR_INPUT_PUMP_ID: Short = 5100 // 0x13ec
