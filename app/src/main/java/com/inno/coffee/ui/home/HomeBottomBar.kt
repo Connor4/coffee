2c3e99e9952d30ee01e36d68f22c35138520f4d9
zbdeng <dengzhibin@innoinstrument.com.cn>
2024-09-11 19:42:08 +0800

    调整代码

--- app/src/main/java/com/inno/coffee/ui/home/HomeBottomBar.kt
+++ app/src/main/java/com/inno/coffee/ui/home/HomeBottomBar.kt
@@ -24,7 +24,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
@@ -51,7 +50,6 @@ fun HomeBottomBar(
     val coffeeHeating by SelfCheckManager.coffeeHeating.collectAsState()
     val steamHeating by SelfCheckManager.steamHeating.collectAsState()
     val warningList by GlobalDialogManager.getInstance().warningExist.collectAsState()
-    val context = LocalContext.current
 
     Box(
         modifier = Modifier
