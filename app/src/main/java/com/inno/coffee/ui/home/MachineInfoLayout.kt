82f9cfaea55578618359f965a58830afd66d00f5
zbdeng <dengzhibin@innoinstrument.com.cn>
2024-09-11 14:33:15 +0800

    修改功能页面全屏

--- app/src/main/java/com/inno/coffee/ui/home/MachineInfoLayout.kt
+++ app/src/main/java/com/inno/coffee/ui/home/MachineInfoLayout.kt
@@ -6,7 +6,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -38,10 +38,10 @@ fun MachineInfoLayout(
 ) {
     Box(
         modifier = Modifier
-//            .fillMaxSize()
-            .fillMaxWidth()
-            .height(800.dp)
-            .padding(top = 60.dp, bottom = 70.dp)
+            .fillMaxSize()
+//            .fillMaxWidth()
+//            .height(800.dp)
+//            .padding(top = 60.dp, bottom = 70.dp)
             .background(Color(0xED000000))
             .clickable(enabled = false) { },
         contentAlignment = Alignment.Center
