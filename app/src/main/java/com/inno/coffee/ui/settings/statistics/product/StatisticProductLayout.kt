b3645933641129769819ff47f645926c2239ced3
zbdeng <dengzhibin@innoinstrument.com.cn>
2024-09-20 11:25:31 +0800

    修改文字居中

--- app/src/main/java/com/inno/coffee/ui/settings/statistics/product/StatisticProductLayout.kt
+++ app/src/main/java/com/inno/coffee/ui/settings/statistics/product/StatisticProductLayout.kt
@@ -41,6 +41,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -134,6 +135,7 @@ fun StatisticProductLayout(
                     text = stringResource(id = stringRes),
                     fontSize = 6.nsp(),
                     color = Color.White,
+                    textAlign = TextAlign.Center,
                     modifier = Modifier
                         .align(Alignment.TopCenter)
                         .offset(y = 129.dp)
