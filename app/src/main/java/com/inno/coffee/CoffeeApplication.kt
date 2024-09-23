c9ed07d098209ac5bde20543a03d6234a4d94d13
zbdeng <dengzhibin@innoinstrument.com.cn>
2024-09-23 19:33:20 +0800

    修复开机马上点击制作饮品，延迟初始化的repository调用空指针问题。去除延迟初始化
    (cherry picked from commit 48fc861448cad84139cf12b89d228b7d9a5af2e7)

--- app/src/main/java/com/inno/coffee/CoffeeApplication.kt
+++ app/src/main/java/com/inno/coffee/CoffeeApplication.kt
@@ -13,7 +13,6 @@ import com.inno.serialport.function.data.DataCenter
 import dagger.hilt.android.HiltAndroidApp
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 @HiltAndroidApp
@@ -40,7 +39,7 @@ class CoffeeApplication : Application() {
             SerialPortDataManager.instance.open()
             DataCenter.init()
 
-            delay(3000)
+//            delay(3000)
             Logger.d(TAG, "CoffeeApplication delayInit start")
             ProductProfileManager.init(this@CoffeeApplication)
             DefaultSettingManager.init(this@CoffeeApplication)
