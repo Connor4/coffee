660bf14b6bbee513c25b46f4ab14cc87fe9f1f50
zbdeng <dengzhibin@innoinstrument.com.cn>
2024-09-23 15:13:30 +0800

    添加异常弹窗多语言适配

--- app/src/main/java/com/inno/coffee/ui/notice/GlobalDialogManager.kt
+++ app/src/main/java/com/inno/coffee/ui/notice/GlobalDialogManager.kt
@@ -66,7 +66,9 @@ class GlobalDialogManager private constructor(private val application: Applicati
                 if (validDialogData(receivedData.code)) {
                     val dialogData = DialogData().apply {
                         errorCode = receivedData.code
-                        message = "ErrorCode:${errorCode}, Message: ${receivedData.info}"
+                        val detail = application.resources.getString(
+                            serialErrorMap[errorCode] ?: R.string.error_no_resource)
+                        message = "E $errorCode: $detail"
                     }
                     dialogDataList.clear()
                     dialogDataList.add(dialogData)
@@ -83,7 +85,9 @@ class GlobalDialogManager private constructor(private val application: Applicati
                             if (validDialogData(error.status.value)) {
                                 val dialogData = DialogData().apply {
                                     errorCode = error.status.value
-                                    message = "ErrorCode:${error.value}"
+                                    val detail = application.resources.getString(
+                                        machineErrorMap[errorCode] ?: R.string.error_no_resource)
+                                    message = "E $errorCode: $detail"
                                 }
                                 tempList.add(dialogData)
                             }
@@ -184,6 +188,32 @@ class GlobalDialogManager private constructor(private val application: Applicati
         private var INSTANCE: GlobalDialogManager? = null
         private var application: Application? = null
         private const val TAG = "GlobalDialogManager"
+        private val serialErrorMap = mapOf(
+            -1 to R.string.error_serial_read_fail,
+            -2 to R.string.error_max_read_retry,
+            -3 to R.string.error_max_open_retry,
+            -4 to R.string.error_serial_io_exception,
+            -5 to R.string.error_serial_format_invalid,
+            -6 to R.string.error_crc_check_failed,
+            -7 to R.string.error_read_no_data,
+            -8 to R.string.error_heart_beat_miss,
+            -9 to R.string.error_frame_size_error,
+            -10 to R.string.error_serial_no_reply,
+            -11 to R.string.error_command_no_reply,
+        )
+        private val machineErrorMap = mapOf(
+            2000 to R.string.error_front_bean_container_empty,
+            2001 to R.string.error_back_bean_container_empty,
+            2002 to R.string.error_bean_container_empty,
+            3000 to R.string.error_left_grinder_error,
+            3001 to R.string.error_right_grinder_error,
+            3002 to R.string.error_left_boiler_error,
+            3003 to R.string.error_right_boiler_error,
+            3004 to R.string.error_left_brewer_error,
+            3005 to R.string.error_right_brewer_error,
+            3006 to R.string.error_steam_boiler_error,
+            4000 to R.string.error_no_water,
+        )
 
         fun init(context: Application) {
             application = context
