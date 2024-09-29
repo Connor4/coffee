8792df1363fd37822002e58db2d456ae8d1a397e
zbdeng <dengzhibin@innoinstrument.com.cn>
2024-09-29 09:43:20 +0800

    修改日志输出

--- serialport/src/main/java/com/inno/serialport/function/chain/MakeDrinkReplyChain.kt
+++ serialport/src/main/java/com/inno/serialport/function/chain/MakeDrinkReplyChain.kt
@@ -9,16 +9,15 @@ import com.inno.serialport.utilities.ReceivedData
 
 class MakeDrinkReplyChain : Chain() {
     override fun canHandle(pullBufInfo: PullBufInfo): Boolean {
-        Logger.d("canHandle make")
         return MAKE_DRINKS_COMMAND_ID == pullBufInfo.command
     }
 
     override fun handle(pullBufInfo: PullBufInfo): ReceivedData? {
-        Logger.d("replay  ${pullBufInfo.pollBuf.toHexString()}")
+        Logger.d("MakeDrinkReplyChain", "replay  ${pullBufInfo.pollBuf.toHexString()}")
         val makeDrinkReply = ReceivedData.HeartBeat()
         val data = pullBufInfo.pollBuf
         val value = ((data[1].toInt() and 0xFF) shl 8) or (data[0].toInt() and 0xFF)
-        Logger.d("value $value")
+        Logger.d("MakeDrinkReplyChain", "value $value")
         makeDrinkReply.makeDrinkReply = HeartBeatReply.MakeDrinkReplay(100, value)
         return makeDrinkReply
     }
