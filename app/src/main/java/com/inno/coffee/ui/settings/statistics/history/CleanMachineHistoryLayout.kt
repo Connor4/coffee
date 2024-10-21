e7c323a05b5413cc68bc23379e20b45bede9bafc
zbdeng <dengzhibin@innoinstrument.com.cn>
2024-10-21 10:17:27 +0800

    修改洗机、产品统计页面绑定viewmodel

--- app/src/main/java/com/inno/coffee/ui/settings/statistics/history/CleanMachineHistoryLayout.kt
+++ app/src/main/java/com/inno/coffee/ui/settings/statistics/history/CleanMachineHistoryLayout.kt
@@ -10,29 +10,34 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.inno.coffee.R
 import com.inno.coffee.utilities.nsp
+import com.inno.coffee.viewmodel.settings.statistics.ProductHistoryViewModel
 import com.inno.common.db.entity.CleanMachineHistory
 
 
 @Composable
 fun CleanMachineHistoryLayout(
+    viewModel: ProductHistoryViewModel = hiltViewModel(),
     onCloseClick: () -> Unit = {},
 ) {
-    val list = mutableListOf<CleanMachineHistory>()
+    val historyList by viewModel.cleanHistory.collectAsState()
     val placeHolder = CleanMachineHistory()
 
     CommonHistoryListLayout(
         listPaddingTop = 202,
         title = stringResource(R.string.statistic_clean_history),
         placeHolder = placeHolder,
-        list = list,
+        list = historyList,
         onCloseClick = { onCloseClick() },
         header = {
             Box(
--- app/src/main/java/com/inno/coffee/ui/settings/statistics/history/ProductHisotryLayout.kt
+++ app/src/main/java/com/inno/coffee/ui/settings/statistics/history/ProductHisotryLayout.kt
@@ -10,28 +10,33 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.inno.coffee.R
 import com.inno.coffee.utilities.nsp
+import com.inno.coffee.viewmodel.settings.statistics.ProductHistoryViewModel
 import com.inno.common.db.entity.ProductHistory
 
 @Composable
 fun ProductHistoryLayout(
+    viewModel: ProductHistoryViewModel = hiltViewModel(),
     onCloseClick: () -> Unit = {},
 ) {
-    val list = mutableListOf<ProductHistory>()
+    val drinksHistoryList by viewModel.productHistory.collectAsState()
     val placeHolder = ProductHistory()
 
     CommonHistoryListLayout(
         listPaddingTop = 236,
         title = stringResource(R.string.statistic_product_history),
         placeHolder = placeHolder,
-        list = list,
+        list = drinksHistoryList,
         onCloseClick = { onCloseClick() },
         header = {
             Box(
