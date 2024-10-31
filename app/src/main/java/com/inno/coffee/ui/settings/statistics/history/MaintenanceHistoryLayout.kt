4cbc968191a8609428e14256880ac9db2127ee09
zbdeng <dengzhibin@innoinstrument.com.cn>
2024-10-31 09:01:46 +0800

    去除测试代码

--- app/src/main/java/com/inno/coffee/ui/settings/statistics/history/MaintenanceHistoryLayout.kt
+++ app/src/main/java/com/inno/coffee/ui/settings/statistics/history/MaintenanceHistoryLayout.kt
@@ -100,7 +100,7 @@ fun MaintenanceHistoryLayout(
                 onEnterClick = { description ->
                     showInput = false
                     viewModel.addMaintenanceHistory(description)
-                    viewModel.addFakeHistoryDataForTest()
+//                    viewModel.addFakeHistoryDataForTest()
                 },
                 onCloseClick = {
                     showInput = false
