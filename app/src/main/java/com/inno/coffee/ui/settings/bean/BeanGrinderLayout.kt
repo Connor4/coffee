ee58935ca38fe2a2b109c0d3906472ccab92a5d1
zbdeng <dengzhibin@innoinstrument.com.cn>
2024-11-07 14:16:01 +0800

    添加豆缸磨刀页面页面选择逻辑

--- app/src/main/java/com/inno/coffee/ui/settings/bean/BeanGrinderLayout.kt
+++ app/src/main/java/com/inno/coffee/ui/settings/bean/BeanGrinderLayout.kt
@@ -13,7 +13,9 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -25,8 +27,18 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.inno.coffee.R
 import com.inno.coffee.ui.common.ChangeColorButton
+import com.inno.coffee.ui.common.ListSelectLayout
+import com.inno.coffee.ui.common.UnitValueScrollBar
 import com.inno.coffee.ui.common.fastclick
 import com.inno.coffee.ui.settings.display.DisplayItemLayout
+import com.inno.coffee.utilities.BEAN_KEY_INDEX_ETC_FRONT
+import com.inno.coffee.utilities.BEAN_KEY_INDEX_ETC_REAR
+import com.inno.coffee.utilities.BEAN_KEY_INDEX_FRONT_HOPPER
+import com.inno.coffee.utilities.BEAN_KEY_INDEX_GRINDING_CAPACITY_FRONT
+import com.inno.coffee.utilities.BEAN_KEY_INDEX_GRINDING_CAPACITY_REAR
+import com.inno.coffee.utilities.BEAN_KEY_INDEX_LEVELLING
+import com.inno.coffee.utilities.BEAN_KEY_INDEX_PQC
+import com.inno.coffee.utilities.BEAN_KEY_INDEX_REAR_HOPPER
 import com.inno.coffee.utilities.INVALID_INT
 import com.inno.coffee.utilities.nsp
 
@@ -35,6 +47,14 @@ fun BeanGrinderLayout(
     onCloseClick: () -> Unit = {},
 ) {
     val itemSelectIndex = remember { mutableIntStateOf(INVALID_INT) }
+    val defaultValue = remember { mutableStateOf("") }
+    val dataMap = remember { mutableMapOf<String, Any>() }
+    val scrollDefaultValue = remember { mutableStateOf(0f) }
+    val scrollRangeStart = remember { mutableStateOf(0f) }
+    val scrollRangeEnd = remember { mutableStateOf(0f) }
+    val scrollUnit = remember { mutableStateOf("") }
+    val scrollAccuracy = remember { mutableStateOf(1) }
+    val titleValue = remember { mutableStateOf("") }
 
     Box(
         modifier = Modifier
@@ -75,40 +95,70 @@ fun BeanGrinderLayout(
         ) {
             DisplayItemLayout(stringResource(R.string.bean_hopper_rear_name),
                 "", Color(0xFF191A1D)) {
-                itemSelectIndex.value = 1
+                itemSelectIndex.value = BEAN_KEY_INDEX_REAR_HOPPER
             }
             DisplayItemLayout(stringResource(R.string.bean_hopper_front_name),
                 "", Color(0xFF2A2B2D)) {
-                itemSelectIndex.value = 1
+                itemSelectIndex.value = BEAN_KEY_INDEX_FRONT_HOPPER
             }
             DisplayItemLayout(stringResource(R.string.bean_levelling),
                 "", Color(0xFF191A1D)) {
-                itemSelectIndex.value = 1
+                itemSelectIndex.value = BEAN_KEY_INDEX_LEVELLING
             }
             Spacer(modifier = Modifier.height(40.dp))
 
             DisplayItemLayout(stringResource(R.string.bean_powder_quantity_control),
                 "", Color(0xFF2A2B2D)) {
-                itemSelectIndex.value = 1
+                itemSelectIndex.value = BEAN_KEY_INDEX_PQC
             }
             DisplayItemLayout(stringResource(R.string.bean_grinding_capacity_hopper_rear),
                 "", Color(0xFF191A1D)) {
-                itemSelectIndex.value = 1
+                itemSelectIndex.value = BEAN_KEY_INDEX_GRINDING_CAPACITY_REAR
             }
             DisplayItemLayout(stringResource(R.string.bean_grinding_capacity_hopper_front),
                 "", Color(0xFF2A2B2D)) {
-                itemSelectIndex.value = 1
+                itemSelectIndex.value = BEAN_KEY_INDEX_GRINDING_CAPACITY_FRONT
             }
             DisplayItemLayout(stringResource(R.string.bean_extraction_time_control_rear),
                 "", Color(0xFF191A1D)) {
-                itemSelectIndex.value = 1
+                itemSelectIndex.value = BEAN_KEY_INDEX_ETC_REAR
             }
             DisplayItemLayout(stringResource(R.string.bean_extraction_time_control_front),
                 "", Color(0xFF2A2B2D)) {
-                itemSelectIndex.value = 1
+                itemSelectIndex.value = BEAN_KEY_INDEX_ETC_FRONT
             }
         }
 
+        if (itemSelectIndex.value != INVALID_INT) {
+            if (itemSelectIndex.value == BEAN_KEY_INDEX_GRINDING_CAPACITY_REAR ||
+                    itemSelectIndex.value == BEAN_KEY_INDEX_GRINDING_CAPACITY_FRONT) {
+                key(scrollDefaultValue.value) {
+                    UnitValueScrollBar(
+                        modifier = Modifier
+                            .align(Alignment.TopEnd)
+                            .padding(top = 172.dp, end = 90.dp)
+                            .width(550.dp)
+                            .wrapContentHeight(),
+                        value = scrollDefaultValue.value,
+                        rangeStart = scrollRangeStart.value,
+                        rangeEnd = scrollRangeEnd.value,
+                        unit = scrollUnit.value,
+                        accuracy = scrollAccuracy.value
+                    ) { changeValue ->
+//                        viewModel.saveMachineParamsValue(itemSelectIndex.value, changeValue)
+                    }
+                }
+            } else {
+                ListSelectLayout(titleValue.value, defaultValue.value, dataMap.toMap(),
+                    { _, value ->
+//                        viewModel.saveMachineParamsValue(itemSelectIndex.value, value)
+                        itemSelectIndex.value = INVALID_INT
+                    }, {
+                        itemSelectIndex.value = INVALID_INT
+                    }
+                )
+            }
+        }
     }
 
 }
