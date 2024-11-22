7805a61f9efca06db58b9fe336786cb4296e0e1b
zbdeng <dengzhibin@innoinstrument.com.cn>
2024-11-22 17:58:11 +0800

    添加注释

--- app/src/main/java/com/inno/coffee/function/selfcheck/SelfCheckManager.kt
+++ app/src/main/java/com/inno/coffee/function/selfcheck/SelfCheckManager.kt
@@ -16,6 +16,7 @@ import kotlinx.coroutines.launch
 // 1. IO自动检测状态，通过pull返回结果，同时根据pull返回IO自检阶段。
 // 2. 自检完成由应用触发冲水等流程。
 // 3. 制作异常，需要取消并执行清除操作。清除操作可细分各个阶段，磨粉阶段丢弃粉即可，萃取阶段需要完成萃取。
+// TODO 4. 下发磨刀模块数据
 object SelfCheckManager {
     const val RELEASE_STEAM_READY = 1
     const val RELEASE_STEAM_START = 2
