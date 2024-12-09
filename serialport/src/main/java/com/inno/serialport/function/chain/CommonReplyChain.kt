a7f741bc5ea296a0ccd183cc5b7ca6433d5776f4
zbdeng <dengzhibin@innoinstrument.com.cn>
2024-12-09 17:56:22 +0800

    更新通用命令范围

--- serialport/src/main/java/com/inno/serialport/function/chain/CommonReplyChain.kt
+++ serialport/src/main/java/com/inno/serialport/function/chain/CommonReplyChain.kt
@@ -2,13 +2,13 @@ package com.inno.serialport.function.chain
 
 import com.inno.common.utils.Logger
 import com.inno.common.utils.toHexString
+import com.inno.serialport.utilities.GRINDER_ADJ_GRIND_ID
 import com.inno.serialport.utilities.MACHINE_PARAM_COMMAND_ID
-import com.inno.serialport.utilities.MACHINE_TEST_AIR_PUMP
 import com.inno.serialport.utilities.PullBufInfo
 import com.inno.serialport.utilities.ReceivedData
 
 class CommonReplyChain : Chain() {
-    private val commonReplyCommandList = MACHINE_PARAM_COMMAND_ID until MACHINE_TEST_AIR_PUMP + 1
+    private val commonReplyCommandList = MACHINE_PARAM_COMMAND_ID until GRINDER_ADJ_GRIND_ID + 1
 
     override fun canHandle(pullBufInfo: PullBufInfo): Boolean {
         return pullBufInfo.command in commonReplyCommandList
