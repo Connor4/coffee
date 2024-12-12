af15a6b039fb27ce9bf03615a2b7819fbe06984b
zbdeng <dengzhibin@innoinstrument.com.cn>
2024-12-12 09:54:08 +0800

    添加奶产品数据结构跟id

--- serialport/src/main/java/com/inno/serialport/utilities/profile/ComponentConst.kt
+++ serialport/src/main/java/com/inno/serialport/utilities/profile/ComponentConst.kt
@@ -2,11 +2,14 @@ package com.inno.serialport.utilities.profile
 
 const val LEFT_BREWER_ID: Short = 1000 // 0x3e8
 const val RIGHT_BREWER_ID: Short = 1001 // 0x3e9
+
 const val LEFT_BOILER_ID: Short = 2000 // 0x7d0
 const val RIGHT_BOILER_ID: Short = 2001 // 0x7d1
 const val STEAM_BOILER_ID: Short = 2002 // 0x7d2
+
 const val FRONT_GRINDER_ID: Short = 3000 // 0xbb8
 const val BACK_GRINDER_ID: Short = 3001 //0xbb9
+
 const val WATER_INPUT_VALVE_ID: Short = 4000 // 0xfa0
 const val LEFT_VALVE_LEFT_BOILER_ID: Short = 4001 // 0xfa1
 const val MIDILE_VALVE_LEFT_BOILER_ID: Short = 4002 // 0xfa2
@@ -14,5 +17,17 @@ const val RIGHT_VALVE_LEFT_BOILER_ID: Short = 4003 // 0xfa3
 const val LEFT_VALVE_RIGHT_BOILER_ID: Short = 4004 // 0xfa4
 const val MIDILE_VALVE_RIGHT_BOILER_ID: Short = 4005 // 0xfa5
 const val RIGHT_VALVE_RIGHT_BOILER_ID: Short = 4006 // 0xfa6
+const val STEAM_PURGE_VALVE_ID = 4007 // 0xfa7
+const val STEAM_PURGE_MIX_VALVE_ID = 4008 // 0xfa8
+const val STEAM_WATER_FILL_VALVE_ID = 4009 // 0xfa9
+const val STEAM_HOT_WATER_VALVE_ID = 4010 // 0xfaa
+const val STEAM_HOT_WATER_VALVE_MIX_ID = 4011 // 0xfab
+const val STEAM_OUT_STEAM_VALVE1_ID = 4012 // 0xfac
+const val STEAM_OUT_STEAM_VALVE2_ID = 4013 // 0xfad
+const val STEAM_EVER_FOAM_VALVE1_ID = 4014 // 0xfae
+const val STEAM_EVER_FOAM_VALVE2_ID = 4015 // 0xfaf
+
 const val WATER_INPUT_PUMP_ID: Short = 5000 // 0x1388
 const val AIR_INPUT_PUMP_ID: Short = 5100 // 0x13ec
+
+const val MILK_FOAMER_ID = 6000 // 0x1770
--- serialport/src/main/java/com/inno/serialport/utilities/profile/Profile.kt
+++ serialport/src/main/java/com/inno/serialport/utilities/profile/Profile.kt
@@ -10,7 +10,7 @@ sealed class Profile {
         val reserve1: Int,
         val reserve2: Int,
         val reserve3: Int,
-        val reserve4: Int
+        val reserve4: Int,
     ) : Profile()
 
     @Serializable
@@ -19,8 +19,8 @@ sealed class Profile {
         val preMakeTime: Int, // 预泡时间
         val postPreMakeWaitTime: Int, // 预泡后停水时间
         val secPressWeight: Int, // 二次压粉程度
-        val reserve0: Int,
-        val reserve1: Int
+        val brewerAdjParam: Int, // 酿造调整参数 brewer adj param
+        val reserve1: Int,
     ) : Profile()
 
     @Serializable
@@ -32,6 +32,30 @@ sealed class Profile {
         // 0:one flow; 1:two flow, water first, coffee second;
         // 2:two flow, coffee first, water second
         val reserve0: Int,
-        val reserve1: Int
+        val reserve1: Int,
     ) : Profile()
+
+    @Serializable
+    data class SteamBoilerProfile(
+        val duration: Int, // steam duration
+        val temperature: Int, // wand temperature
+        val mixHotWater: Int, // if mix hot water product
+        val cleanWand: Int, // if clean wand product
+        val reserve0: Int,
+        val reserve1: Int,
+    ) : Profile()
+
+    @Serializable
+    data class MilkFoamProfile(
+        val appearance: Int, // 0:white on top, 1:brown on top
+        val milkOutput: Int, // 0:coffee outlet, 1:milk arm
+        val milkDelayTime: Int, // ms
+        val milkQuantity1: Int, // 0-100
+        val milkTemperature1: Int, // 0:cold 1:warm
+        val foamTexture1: Int, // 0-100
+        val milkQuantity2: Int, // 0-100
+        val milkTemperature2: Int, // 0:cold 1:warm
+        val foamTexture2: Int, // 0-100
+    ) : Profile()
+
 }
