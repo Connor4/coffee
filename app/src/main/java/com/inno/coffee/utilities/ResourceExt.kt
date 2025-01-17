0092f79566284c4e409c028e95d9eb6412b5f7aa
zbdeng <dengzhibin@innoinstrument.com.cn>
2025-01-17 15:18:37 +0800

    代码增加容错

--- app/src/main/java/com/inno/coffee/utilities/ResourceExt.kt
+++ app/src/main/java/com/inno/coffee/utilities/ResourceExt.kt
@@ -2,15 +2,18 @@ package com.inno.coffee.utilities
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import com.inno.coffee.R
 
 @Composable
 fun getImageResId(imageName: String): Int {
     val context = LocalContext.current
-    return context.resources.getIdentifier(imageName, "drawable", context.packageName)
+    val resId = context.resources.getIdentifier(imageName, "drawable", context.packageName)
+    return if (resId != 0) resId else R.drawable.drink_item_empty_ic
 }
 
 @Composable
 fun getStringResId(stringName: String): Int {
     val context = LocalContext.current
-    return context.resources.getIdentifier(stringName, "string", context.packageName)
+    val resId = context.resources.getIdentifier(stringName, "string", context.packageName)
+    return if (resId != 0) resId else R.string.common_empty_string
 }
