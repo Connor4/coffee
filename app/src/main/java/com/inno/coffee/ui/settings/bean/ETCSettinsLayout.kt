474453fcfe7f7672b920bd31972381f03e21ad68
zbdeng <dengzhibin@innoinstrument.com.cn>
2025-02-06 10:54:26 +0800

    添加萃取设置页面

---
+++ app/src/main/java/com/inno/coffee/ui/settings/bean/ETCSettinsLayout.kt
@@ -0,0 +1,15 @@
+package com.inno.coffee.ui.settings.bean
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ETCSettingsLayout() {
+
+}
+
+@Preview
+@Composable
+private fun ETCSettingsLayoutPreview() {
+    ETCSettingsLayout()
+}
