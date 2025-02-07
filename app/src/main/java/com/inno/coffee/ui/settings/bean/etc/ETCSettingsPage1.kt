58bb62b27bd4232a726a304d48ff4a86deb8c613
zbdeng <dengzhibin@innoinstrument.com.cn>
2025-02-07 15:36:23 +0800

    添加etc设置页面

--- app/src/main/java/com/inno/coffee/ui/settings/bean/etc/ETCSettingsPage1.kt
+++ app/src/main/java/com/inno/coffee/ui/settings/bean/etc/ETCSettingsPage1.kt
@@ -1,11 +1,17 @@
 package com.inno.coffee.ui.settings.bean.etc
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.inno.coffee.utilities.nsp
 
 @Composable
 fun ETCSettingsPage1() {
-
+    Box() {
+        Text(text = "", fontSize = 6.nsp(), color = Color.White)
+    }
 }
 
 @Preview
