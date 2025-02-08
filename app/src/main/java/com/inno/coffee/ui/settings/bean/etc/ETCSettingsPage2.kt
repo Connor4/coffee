0f57de8e13f07c47a4cc87c4dd8fffabc3d5032c
zbdeng <dengzhibin@innoinstrument.com.cn>
2025-02-08 19:13:50 +0800

    etc设置页面第二页

--- app/src/main/java/com/inno/coffee/ui/settings/bean/etc/ETCSettingsPage2.kt
+++ app/src/main/java/com/inno/coffee/ui/settings/bean/etc/ETCSettingsPage2.kt
@@ -1,22 +1,34 @@
 package com.inno.coffee.ui.settings.bean.etc
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.inno.coffee.R
 import com.inno.coffee.function.display.ScreenDisplayManager
 import com.inno.coffee.ui.settings.formula.DrinkPager
 import com.inno.coffee.ui.settings.formula.FormulaValueItem
 import com.inno.coffee.utilities.FORMULA_SHOW_LEARN_WATER
 import com.inno.coffee.utilities.FORMULA_SHOW_POWDER_TEST
+import com.inno.coffee.utilities.nsp
 import com.inno.coffee.viewmodel.settings.formula.FormulaViewModel
 
 
@@ -41,6 +53,19 @@ fun ETCSettingsPage2(
     Box(
         modifier = Modifier.fillMaxSize()
     ) {
+        Text(text = stringResource(R.string.bean_etc_settings_2_front_bean_notice),
+            fontSize = 5.nsp(), color = Color.White, modifier = Modifier
+                .padding(start = 90.dp, top = 120.dp)
+                .width(269.dp)
+                .wrapContentHeight()
+        )
+        Image(painter = painterResource(id = R.drawable.grinder_exam_ic),
+            modifier = Modifier
+                .padding(start = 440.dp, top = 40.dp)
+                .width(216.dp)
+                .height(226.dp),
+            contentDescription = null
+        )
         DrinkPager(selectFormula, totalCount, pagerState, drinksTypeList, 300) {
             viewModel.getFormula(it.productId)
         }
