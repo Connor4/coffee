3055979417239b48be06733bfa075bdb3ed83a81
zbdeng <dengzhibin@innoinstrument.com.cn>
2025-02-12 13:25:45 +0800

    优化代码逻辑

--- serialport/src/main/java/com/inno/serialport/function/chain/HeartBeatChain.kt
+++ serialport/src/main/java/com/inno/serialport/function/chain/HeartBeatChain.kt
@@ -31,36 +31,44 @@ class HeartBeatChain : Chain() {
             val id = ((data[i + 1].toInt() and 0xFF) shl 8) or (data[i].toInt() and 0xFF)
             val value = ((data[i + 3].toInt() and 0xFF) shl 8) or (data[i + 2].toInt() and 0xFF)
 
-            if ((id >= MakeDrinkStatusEnum.LEFT_BREWING.value) and (id <= MakeDrinkStatusEnum
-                        .RIGHT_BREW_COMPLETED.value)) { // 1000 - 1003
-                val drinkStatus = MakeDrinkStatusEnum.getStatus(id)
-                info.makeDrinkStatus = HeartBeatReply.MakeDrinkStatus(drinkStatus, value)
-
-            } else if ((id >= MakeDrinkStatusEnum.LEFT_FINISHED.value) and
-                    (id <= MakeDrinkStatusEnum.RIGHT_FINISHED.value)) { // 1004 - 1005
-                val drinkStatus = MakeDrinkStatusEnum.getStatus(id)
-                val params = data.sliceArray(4 until data.size - 1)
-                info.makeDrinkStatus = HeartBeatReply.MakeDrinkStatus(drinkStatus, value, params)
-
-            } else if (id == BoilerStatusEnum.BOILER_TEMPERATURE.value) { // 1006
-                val temperature = BoilerStatusEnum.getStatus(id)
-                val params = data.sliceArray(2 until data.size - 1)
-                info.temperature = HeartBeatReply.BoilerTemperature(temperature, params)
-
-            } else if ((id == GrinderStatusEnum.LEFT_GRINDER_POWDER_DOSAGE.value) and
-                    (id <= GrinderStatusEnum.RIGHT_GRINDER_POWDER_DOSAGE.value)) { // 1009 - 1010
-                val grinderStatus = GrinderStatusEnum.getStatus(id)
-                info.grinder = HeartBeatReply.GrinderPowderDosage(grinderStatus, value)
-
-            } else if ((id == CleanMachineEnum.CLEAN_COFFEE_FINISH.value) and
-                    (id <= CleanMachineEnum.CLEAN_FOAM_FINISH.value)) {
-                val status = CleanMachineEnum.getStatus(id)
-                info.cleanMachine = HeartBeatReply.CleanMachineStatus(status)
-            } else if ((id >= ErrorStatusEnum.FRONT_VAT_EMPTY.value) and
-                    (id <= ErrorStatusEnum.NO_WATER_ERROR.value)) { // 2000 - 4000
-                val error = ErrorStatusEnum.getStatus(id)
-                info.error = HeartBeatReply.Error(error, value)
-
+            when (id) {
+                in MakeDrinkStatusEnum.LEFT_BREWING.value..
+                        MakeDrinkStatusEnum.RIGHT_BREW_COMPLETED.value,
+                    -> {// 1000 - 1003
+                    val drinkStatus = MakeDrinkStatusEnum.getStatus(id)
+                    info.makeDrinkStatus = HeartBeatReply.MakeDrinkStatus(drinkStatus, value)
+                }
+                in MakeDrinkStatusEnum.LEFT_FINISHED.value..
+                        MakeDrinkStatusEnum.RIGHT_FINISHED.value,
+                    -> { // 1004 - 1005
+                    val drinkStatus = MakeDrinkStatusEnum.getStatus(id)
+                    val params = data.sliceArray(4 until data.size - 1)
+                    info.makeDrinkStatus =
+                        HeartBeatReply.MakeDrinkStatus(drinkStatus, value, params)
+                }
+                BoilerStatusEnum.BOILER_TEMPERATURE.value -> { // 1006
+                    val temperature = BoilerStatusEnum.getStatus(id)
+                    val params = data.sliceArray(2 until data.size - 1)
+                    info.temperature = HeartBeatReply.BoilerTemperature(temperature, params)
+                }
+                in GrinderStatusEnum.LEFT_GRINDER_POWDER_DOSAGE.value..
+                        GrinderStatusEnum.RIGHT_GRINDER_POWDER_DOSAGE.value,
+                    -> {// 1009 - 1010
+                    val grinderStatus = GrinderStatusEnum.getStatus(id)
+                    info.grinder = HeartBeatReply.GrinderPowderDosage(grinderStatus, value)
+                }
+                in CleanMachineEnum.CLEAN_COFFEE_FINISH.value..
+                        CleanMachineEnum.CLEAN_FOAM_FINISH.value,
+                    -> {
+                    val status = CleanMachineEnum.getStatus(id)
+                    info.cleanMachine = HeartBeatReply.CleanMachineStatus(status)
+                }
+                in ErrorStatusEnum.FRONT_VAT_EMPTY.value..
+                        ErrorStatusEnum.NO_WATER_ERROR.value,
+                    -> { // 2000 - 4000
+                    val error = ErrorStatusEnum.getStatus(id)
+                    info.error = HeartBeatReply.Error(error, value)
+                }
             }
 
             heartBeatList.list.add(info)
