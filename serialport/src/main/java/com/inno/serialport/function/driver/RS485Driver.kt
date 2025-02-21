dc229a2baaac60cea560f9910bfaf313179075ba
zbdeng <dengzhibin@innoinstrument.com.cn>
2025-02-21 14:24:49 +0800

    修改驱动位置

--- serialport/src/main/java/com/inno/serialport/function/driver/RS485Driver.kt
+++ serialport/src/main/java/com/inno/serialport/function/driver/RS485Driver.kt
@@ -21,7 +21,7 @@ import java.nio.ByteBuffer
 import java.nio.ByteOrder
 
 class RS485Driver(
-    devicePath: String = "/dev/ttyS4",
+    devicePath: String = "/dev/ttyS0",
     baudRate: Int = 115200,
     dataBits: Int = DATA_BITS_8,
     stopBits: Int = STOP_BITS_1,
